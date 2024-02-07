a5d6856f353d15e5c479f88c9097f4a8d1a64cfa
Bruno Giardina <66431858+brunogiardina@users.noreply.github.com>
2024-02-07 03:41:09 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('section a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset
+    }, 500);
+})
