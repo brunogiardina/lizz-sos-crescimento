cf77e90968044790ea0c5435c3b10a48dfccdf2f
Bruno Giardina <66431858+brunogiardina@users.noreply.github.com>
2024-02-08 05:09:08 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$('.purchase-link').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset
+    }, 500);
+});
+
+$('.contagem').each(function() {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
