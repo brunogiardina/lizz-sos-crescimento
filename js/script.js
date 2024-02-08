f1ff075aeb25501fd52371494c8268385b51e75b
Bruno Giardina <66431858+brunogiardina@users.noreply.github.com>
2024-02-08 04:54:14 +0000

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$('section a').click(function(e){
-    e.preventDefault();
-    var id = $(this).attr('href'),
-        targetOffset = $(id).offset().top;
-    $('html, body').animate({
-        scrollTop: targetOffset
-    }, 500);
-})
